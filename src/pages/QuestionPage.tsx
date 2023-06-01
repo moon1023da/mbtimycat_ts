5fc687944c270feb92e120d972b3032214c56244
moon1023 <moon1023da@naver.com>
2023-06-01 14:35:49 +0900

    질문페이지

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 
 import { QuestionData } from "../stores/Question/QuestionData";
 import Header from '../components/Header';
 
+
 function QuestionPage(): React.ReactElement{
 
-  const [questionNo, setQuestionNo] =React.useState(0);
+  const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore, setTotalScore] = React.useState([
+    {id:"EI", score: 0},
+    {id:"SN", score: 0},
+    {id:"TF", score: 0},
+    {id:"JP", score: 0},
+  ]);
+  const navigate = useNavigate();
 
-  const handleCickAnswer = () => {
-    setQuestionNo(questionNo +1);
+  const handleCickAnswer = (ans: number, type: string) => {
+    const newScore = totalScore.map(s=> 
+      s.id === type ? {id:s.id, score: s.score = ans} : s,
+    );
+
+    setTotalScore(newScore);
+      // 마지막 문제가 아닐경우
+    if (QuestionData.length !== questionNo +1) {
+      setQuestionNo(questionNo + 1);
+      // 마지막 문제일 경우
+    } else {
+      navigate("/result");
+    }
+
   };
+
 
   return (
     <Wrapper>
@@ -19,8 +41,32 @@ function QuestionPage(): React.ReactElement{
         <Header type="progress" questionNo={questionNo}/>
         <Title>{QuestionData[questionNo].title}</Title>
         <ButtonGroup>
-          <Button className="btn-warning" style={{marginRight: "20px"}} onClick={handleCickAnswer}>{QuestionData[questionNo].anwsera}</Button>
-          <Button className="btn-warning">{QuestionData[questionNo].anwserb}</Button>
+          <Button className="btn-warning" 
+          style={{
+            marginRight:"20px",
+            width: "45%",
+            minHeight: "200px",
+            fontSize: "15pt",
+          }} 
+          onClick={() =>
+            handleCickAnswer(1, QuestionData[questionNo].type)
+            }
+          >
+            {QuestionData[questionNo].anwsera}
+          </Button>
+          <Button className="btn-warning" 
+          style={{
+            marginRight:"20px",
+            width: "45%",
+            minHeight: "200px",
+            fontSize: "15pt",
+          }} 
+          onClick={() =>
+            handleCickAnswer(2, QuestionData[questionNo].type)
+          }
+          >
+            {QuestionData[questionNo].anwserb}
+          </Button>
         </ButtonGroup>
       </ContentsWrapper>
     </Wrapper>
@@ -51,6 +97,8 @@ const ContentsWrapper = styled.div`
 const Title = styled.div`
   margin-top: 20px;
   font-size: 25pt;
+  min-width: 380px;
+  text-align: center;
 
 `;
 
@@ -61,6 +109,6 @@ const ButtonGroup = styled.div`
  margin-top: 20px;
  font-size: 25pt;
  button{
-  font-size: 18pt;
+  font-size: 16pt;
  };
 `
